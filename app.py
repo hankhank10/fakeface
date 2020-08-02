5e1deb7fc8cc6bf48f42177abe2a695aaf79469d
Mark Hank <mark@secureup.link>
2020-08-02 17:17:09 +0100

    Latest

--- app.py
+++ app.py
@@ -1,18 +1,104 @@
-from flask import Flask
-import faces
+from flask import Flask, jsonify
+import requests
+import shutil
+import cv2
+import secrets
+from pyagender import PyAgender
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+class ImageRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gender = db.Column(db.String(10))
+    age = db.Column(db.Integer)
+    filename = db.Column(db.String(100))
+    date_added = db.Column(db.DateTime)
+    source = db.Column(db.String(100))
+
+# settings
+url = "https://thispersondoesnotexist.com/image"
+male_threshold = 0.4
+female_threshold = 0.6
+temp_file = "temp_img.jpg"
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/get_face')
-def get_face():
-    faces.download_face()
-    gender, age = faces.recoginise_face()
+
+@app.route('/download_face')
+def download_face():
+    # download face
+    response = requests.get(url, stream=True)
+    with open(temp_file, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+
+    return "Downloaded to temporary file"
+
+
+@app.route('/recognise_face')
+def recognise_face():
+    agender = PyAgender()
+
+    faces = agender.detect_genders_ages(cv2.imread(temp_file))
+    if len(faces) == 1:
+        face = faces[0]
+        gender_numeric = face['gender']
+        age = int(face['age'])
+
+        gender = "unclear"
+        if gender_numeric < male_threshold: gender = "male"
+        if gender_numeric > male_threshold: gender = "female"
+    else:
+        #face not detected or multiple faces detected
+        gender = "unclear"
+        age = 0
+
+    return gender, age
+
+
+@app.route('/generate_face')
+def generate_face():
+    download_face()
+    gender, age = recognise_face()
+
+    image_record = ImageRecord(
+        gender=gender,
+        age=age,
+        filename="Test",
+        date_added=datetime.utcnow())
+
+    db.session.add(image_record)
+    db.session.commit()
+
+    location_to_move_to = "classified/" + gender + "_" + str(age) + "_" + secrets.token_hex(20) + ".jpg"
+    shutil.move(temp_file, location_to_move_to)
+
     return gender
 
 
+@app.route('/test')
+def test():
+    db_output = ImageRecord.query.first()
+    dict_output = {
+        'id': db_output.id,
+        'gender': db_output.gender,
+        'age': db_output.age,
+        'filename': db_output.filename,
+        'date_added': db_output.date_added
+    }
+    return jsonify(dict_output)
+
+
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(host='0.0.0.0', port=11000, debug=True)
--- generate_faces.py
+++ generate_faces.py
@@ -11,7 +11,8 @@ from time import sleep
 url = "https://thispersondoesnotexist.com/image"
 male_threshold = 0.4
 female_threshold = 0.6
-temp_file = "temp_img.jpg"
+temp_file = "img.jpg"
+times_to_run = 100
 seconds_to_sleep = 1
 
 
@@ -21,30 +22,32 @@ def download_face():
         shutil.copyfileobj(response.raw, out_file)
     return
 
+
 def recoginise_face():
     faces = agender.detect_genders_ages(cv2.imread(temp_file))
-    face = faces[0]
-    gender_numeric = face['gender']
-    age = int(face['age'])
+    if len(faces) == 1:
+        face = faces[0]
+        gender_numeric = face['gender']
+        age = int(face['age'])
 
-    gender = "unclear"
-    if gender_numeric < male_threshold: gender = "male"
-    if gender_numeric > male_threshold: gender = "female"
+        gender = "unclear"
+        if gender_numeric < male_threshold: gender = "male"
+        if gender_numeric > male_threshold: gender = "female"
+    else:
+        #face not detected or multiple faces detected
+        gender = "unclear"
+        age = 0
 
     return gender, age
 
-def move_file(gender, age):
-    category = "older"
-    if age < 50: category = "adult"
-    if age < 30: category = "young_adult"
-    if age < 20: category = "child"
 
-    if category == "older":
-        location_to_move_to = gender + "/" + gender + "/" + category + "/" + gender + "_" + str(age) + "_" + secrets.token_hex(10) + ".jpg"
-        shutil.move(temp_file, location_to_move_to)
-    else:
-        print ("Wrong category, so skipping save")
-    return
+def move_file(gender, age):
+    filename = gender + "_" + str(age) + "_" + secrets.token_hex(20) + ".jpg"
+    location_to_move_to = "static/classified/" + filename
+    shutil.move(temp_file, location_to_move_to)
+    return filename
+
+def write_db(gender, age, filename):
 
 
 agender = PyAgender()
@@ -58,6 +61,7 @@ for a in range(1, times_to_run):
     if gender != "unclear":
         print(str(age) + " year old " + gender)
         move_file(gender, age)
+        write_db (gender, age, filename)
     else:
         print("gender unclear, so skipping")
     sleep(seconds_to_sleep)
